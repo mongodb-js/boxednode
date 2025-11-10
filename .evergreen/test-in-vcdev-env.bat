e51e0b3a46f156c739e89f6c76e5436ee81f8349
Kevin Mas Ruiz <kevin.mas@hey.com>
2025-11-10 15:43:10 +0100

    chore: stub APPDATA env var

--- .evergreen/test-in-vcdev-env.bat
+++ .evergreen/test-in-vcdev-env.bat
@@ -1,4 +1,6 @@
 CALL "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
+REM APPDATA is empty in CMD, and npm requires it to at least be empty
+SET APPDATA=""
 
 npm run build
 npm run test-ci
